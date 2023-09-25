86e8da98931808212f94a8434a9a7e050f5c2fdf
Chang Liu <lchang199x@gmail.com>
2023-09-25 23:25:46 +0800

    add buildSrc

---
+++ buildSrc/src/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public/")
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
+
+// buildSrc build needs to explicitly add kotlin directory to java srcDirs
+sourceSets {
+    main {
+        java.srcDirs("src/main/kotlin")
+    }
+
+    test {
+        java.srcDirs("src/test/kotlin")
+    }
+}
